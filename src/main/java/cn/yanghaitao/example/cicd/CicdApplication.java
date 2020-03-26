0ab8fc93226529c900e1a8493de16be039596e7d
Paul Yang <pyang@PYang-2.local>
2020-03-26 10:49:31 +0800

    change 1

--- src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
+++ src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdApplication {
 
 	public static void main(String[] args) {
-                System.out.print("It really works!")	
+                System.out.print("It indeed works!")	
         	SpringApplication.run(CicdApplication.class, args);
 	}
 
