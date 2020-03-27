0717198c7b7c582727aaa5d772896bb0cc09f149
Poseidon1979 <paul.yang1979@gmail.com>
2020-03-27 11:38:50 +0800

    hh

--- src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
+++ src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdApplication {
 
 	public static void main(String[] args) {
-                System.out.print("Hello World!")
+                System.out.print("Hello World!");
         	SpringApplication.run(CicdApplication.class, args);
 	}
 
