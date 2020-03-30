8bf1f9a77442039818d46cfb06a73ba7593fa9dd
Poseidon1979 <paul.yang1979@gmail.com>
2020-03-30 19:38:40 +0800

    sdfghsdf

--- yamlGenerator.sh
+++ yamlGenerator.sh
@@ -1,9 +1,9 @@
 cp -r ${JENKINS_HOME}/helm_template ${JENKINS_HOME}/${JOB_NAME}_helm_charts
 
-rm ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/*.yaml
+rm ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/*.yaml
 
-sed 's/{{.Values.app.name}}/${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/image.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.git.url}}/${GIT_URL}/g' | sed 's/{{.Values.git.revision}}/${GIT_COMMIT}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-image.yaml
+sed 's/{{.Values.app.name}}/${JOB_NAME}/g' ${JENKINS_HOME}/cicd_sample/helm_template/templates/image.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.git.url}}/${GIT_URL}/g' | sed 's/{{.Values.git.revision}}/${GIT_COMMIT}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-image.yaml
 
-sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/pod.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-pod.yaml
+sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/cicd_sample/templates/pod.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-pod.yaml
 
-sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/service.yaml | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-service.yaml
+sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/cicd_sample/templates/service.yaml | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-service.yaml
