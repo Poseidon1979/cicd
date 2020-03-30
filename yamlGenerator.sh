57032155d35856302cd639028dd23e25dc757ecd
Poseidon1979 <paul.yang1979@gmail.com>
2020-03-30 22:02:32 +0800

    Change pushed

--- yamlGenerator.sh
+++ yamlGenerator.sh
@@ -6,14 +6,8 @@ fi
 
 rm ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/*.yaml
 
-env
+sed "s/{{.Values.app.name}}/${JOB_NAME}/g" ${JENKINS_HOME}/helm_template/cicd_sample/templates/image.yaml | sed "s|{{.Values.app.image}}|${CLIENT_DOCKER_IMAGE}|g" | sed "s|{{.Values.git.url}}|${GIT_URL}|g" | sed "s/{{.Values.git.revision}}/${GIT_COMMIT}/g" >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-image.yaml
 
-image="www"
-#image=${client.docker.image}
-
-
-sed "s/{{.Values.app.name}}/${JOB_NAME}/g" ${JENKINS_HOME}/helm_template/cicd_sample/templates/image.yaml | sed "s|{{.Values.app.image}}|${image}|g" | sed "s|{{.Values.git.url}}|${GIT_URL}|g" | sed "s/{{.Values.git.revision}}/${GIT_COMMIT}/g" >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-image.yaml
-
-sed "s/{{.Values.app.label}}/app:${JOB_NAME}/g" ${JENKINS_HOME}/helm_template/cicd_sample/templates/pod.yaml | sed "s|{{.Values.app.image}}|${image}|g" | sed "s/{{.Values.app.name}}/${JOB_NAME}/g" >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-pod.yaml
+sed "s/{{.Values.app.label}}/app:${JOB_NAME}/g" ${JENKINS_HOME}/helm_template/cicd_sample/templates/pod.yaml | sed "s|{{.Values.app.image}}|${CLIENT_DOCKER_IMAGE}|g" | sed "s/{{.Values.app.name}}/${JOB_NAME}/g" >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-pod.yaml
 
 sed "s/{{.Values.app.label}}/app:${JOB_NAME}/g" ${JENKINS_HOME}/helm_template/cicd_sample/templates/service.yaml | sed "s/{{.Values.service.name}}/${JOB_NAME}/g" >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/cicd_sample/templates/${JOB_NAME}-service.yaml
