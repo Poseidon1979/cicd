93e6cbd7b0f8d9629ddf4d40fb5c8330ce20acf8
Poseidon1979 <paul.yang1979@gmail.com>
2020-03-30 09:58:34 +0800

    Test DiscoveryClient

--- src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
+++ src/main/java/cn/yanghaitao/example/cicd/CicdApplication.java
@@ -1,10 +1,28 @@
 package cn.yanghaitao.example.cicd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CicdApplication {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+
+	private void getServiceInfo()
+	{
+           List<ServiceInstance> instanceList = discoveryClient.getInstances("kubernetes");
+           ServiceInstance instance = instanceList.get(0);
+           System.out.println("the instance name is:" + instance.getUri());
+	}
 
 	public static void main(String[] args) {
                 System.out.print("Hello World!!!");
