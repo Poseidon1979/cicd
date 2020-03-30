a8c0a1d311edee581e27093a5154993ad9d3892b
Poseidon1979 <paul.yang1979@gmail.com>
2020-03-30 19:33:07 +0800

    sdsgg

--- yamlGenerator.sh
+++ yamlGenerator.sh
@@ -1,9 +1,9 @@
-cp -r ${WORKSPACE}/helm_template ${WORKSPACE}/${JOB_NAME}_helm_charts
+cp -r ${JENKINS_HOME}/helm_template ${JENKINS_HOME}/${JOB_NAME}_helm_charts
 
-rm ${WORKSPACE}/${JOB_NAME}_helm_charts/templates/*.yaml
+rm ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/*.yaml
 
-sed 's/{{.Values.app.name}}/${JOB_NAME}/g' ${WORKSPACE}/helm_template/templates/image.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.git.url}}/${GIT_URL}/g' | sed 's/{{.Values.git.revision}}/${GIT_COMMIT}/g' >> ${WORKSPACE}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-image.yaml
+sed 's/{{.Values.app.name}}/${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/image.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.git.url}}/${GIT_URL}/g' | sed 's/{{.Values.git.revision}}/${GIT_COMMIT}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-image.yaml
 
-sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${WORKSPACE}/helm_template/templates/pod.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${WORKSPACE}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-pod.yaml
+sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/pod.yaml | sed 's/{{.Values.app.image}}/${client.docker.image}/g' | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-pod.yaml
 
-sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${WORKSPACE}/helm_template/templates/service.yaml | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${WORKSPACE}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-service.yaml
+sed 's/{{.Values.app.label}}/app=${JOB_NAME}/g' ${JENKINS_HOME}/helm_template/templates/service.yaml | sed 's/{{.Values.app.name}}/${JOB_NAME}/g' >> ${JENKINS_HOME}/${JOB_NAME}_helm_charts/templates/${JOB_NAME}-service.yaml
